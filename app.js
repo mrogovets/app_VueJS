b5655476f98a9cb6589f72384744570303cdfcf5
mrogovets <34492317+mrogovets@users.noreply.github.com>
2021-01-05 23:14:08 +0200

    list of notes (00:57:26)

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const App = {
   data() {
     return {
-      counter: 0,
-      title: "Counter",
+      placeholderString: "Input name of the note",
+      title: "List of notes",
+      inputValue: "",
+      notes: ["Note 1", "Note 2"],
     };
+  },
+  methods: {
+    inputChangeHandler(event) {
+      this.inputValue = event.target.value;
+    },
+    addNewNote() {
+      this.notes.push(this.inputValue);
+      this.inputValue = "";
+    },
+    removeNote(idx) {
+      this.notes.splice(idx, 1); //57:26
+    },
   },
 };
 
+Vue.createApp(App).mount("#app");
+
 // const app = Vue.createApp(App);
 // app.mount("#app");
-
-Vue.createApp(App).mount("#app");
