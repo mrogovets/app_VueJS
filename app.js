18fe3b61da87f9bd050f69fee5e62c7fe81a99f0
mrogovets <34492317+mrogovets@users.noreply.github.com>
2021-01-05 22:16:27 +0200

    counter on Vue

---
+++ app.js
@@ -0,0 +1,13 @@
+const App = {
+  data() {
+    return {
+      counter: 0,
+      title: "Counter",
+    };
+  },
+};
+
+// const app = Vue.createApp(App);
+// app.mount("#app");
+
+Vue.createApp(App).mount("#app");
